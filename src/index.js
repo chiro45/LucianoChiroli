ac508f65e51df13254824b7df4f3556b1fcf6b38
chiro45 <luciano.chiroli45@gmail.com>
2021-12-20 12:55:00 -0300

    translate

--- src/index.js
+++ src/index.js
@@ -72,18 +72,17 @@ darkmode.addEventListener('click',(e)=>{
 })
 //cursor Description
 let str = ` "Soy un apasionado de la informática, en busca de nuevas experiencias, desafíos que me ayuden a obtener conocimientos y practicas para ir incorporando día a día en mi vida"; `;
-let str2 = ` "I am passionate about computing, looking for new experiences, challenges that help me gain knowledge and practices to incorporate day by day into my life"; `;
+let str2 = ` "I am passionate about computing, looking for new experiences, challenges and practices that help me gain knowledge to incorporate day by day into my life"; `;
 const cursor = document.getElementById('description')
 let array = str.split('')
 let array2 = str2.split('')
-let cont = 0;
 let inicio = cursor.innerHTML;
 let conta = 0;
 const escribir = (arr)=>{
     setTimeout(()=>{
         cursor.innerHTML += arr[conta];
         conta++;    
-        if(conta != arr.length){
+        if(conta < arr.length){
            
          escribir(arr)
             
@@ -165,35 +164,96 @@ const leerMas = (span2, leerMas, leerMenos)=>{
 const englishToggle = document.getElementById('idioma')
 englishToggle.addEventListener('click', (e)=>{
         if(!englishToggle.classList.contains("active")){
-            englishToggle.classList.add("active");
-            escribir(array2)
+          englishToggle.classList.add("active");
+            setTimeout(()=>{escribir(array2)},2000)
             //nav
             cambiaIdioma('nav-inicio','<b><</b>Home<b>></b>') 
             cambiaIdioma('nav-sobremi','<b><</b>About Me<b>></b>') 
             cambiaIdioma('nav-tecnologias','<b><</b>Tecnologies<b>></b>') 
             cambiaIdioma('nav-contacto','<b><</b>Contact<b>></b>') 
+            //inicio
             cambiaIdioma("title_descript","<b class='b3'><</b>Hi,I'm Luciano Chiroli<b class='b3'>/></b>")
             cambiaIdioma("contactame","Contact Me <b class='b5'><i class='uil uil-message button__icon'></i></b>")
             cambiaIdioma("sTitle",`<b class='b1'>class =</b> "Frontend Developer";`);
             cambiaIdioma("description","<b class='b3'>let</b> <b class='b2'>description</b> = ")
+            cambiaIdioma("sobreMiTitle",`<b class="b2"><</b>About Me<b class="b2">/></b>`)
+            //description
+            cambiaIdioma("aboutDescription",`I am from Mendoza, Argentina. I am currently 20 years old and I am doing a university degree in programming,`)
+            cambiaIdioma("readMoreSpan",`at the UTN Regional College Mendoza. I am a self-taught person, 
+            persevering and looking for projects that help me grow professionally<b class="b4">"</b>`)
+            cambiaIdioma("readMoreD",`Read More <i class="uil uil-arrow-down"></i>` )
+            cambiaIdioma("readMenosD",`Read Less <i class="uil uil-arrow-up"></i>` )
+            //downloadCv
+            cambiaIdioma("downloadCv",`Download CV <b class="b5"><i class="uil uil-download-alt button__icon"></i></b></a>`)
+            //educations
+            cambiaIdioma("titleEducation",` <b class="b2"><</b>Education<b class="b2">/></b>`)
+            cambiaIdioma("soporteTec","Technical Support, Jr. Achivement 2021/2021")
+            cambiaIdioma("titleCursos",`<b class="b2"><</b>Web courses<b class="b2">/>`)
+            cambiaIdioma("secundario", `Secondary School, Gral Jose De San Martin, N°9-001/2018`)
+            //cursos web
+            cambiaIdioma("leerMasCursos",`Read More <i class="uil uil-arrow-down"></i>` )
+            cambiaIdioma("leerMenosCursos",`Read Less <i class="uil uil-arrow-up"></i>` )
+            cambiaIdioma("tecnoTitle",`<b class="b2"><</b>Tecnologies<b class="b2">/></b>` )
+            cambiaIdioma("descriptTec",`I have knowledge in web layout, user interface development, in current technologies for a frontend developer, such as HTML5, CSS, Bootstrap,`)
+            cambiaIdioma("readMoreSpanT",`JavaScript Vanilla y ReactJs. <br>I also have knowledge on the backend side with node.js and express, in addition to having knowledge of relational databases such as SQL and non-relational databases such as Mongo DB<b class="b4">"</b>`)
+            cambiaIdioma("readMoreMasT",`Read More <i class="uil uil-arrow-down"></i>`)
+            cambiaIdioma("readMenosT",`Read Less <i class="uil uil-arrow-up"></i>`)
+            //contactme
+            cambiaIdioma("contactMe",`<b class="b2"> <</b> Contact Me<b class="b2">/></b>`)
+            cambiaIdioma("labNombre",`Name:`)
+            cambiaIdioma("labAsunto",`Reason:`)
+            cambiaIdioma("labMensaje",`Message:`)
+            cambiaIdioma("buttonSubmit",`Submit<b class="b5"> <i class="uil uil-message button__icon">`)
             
-           
+
         }else{
-        englishToggle.classList.remove("active");
-        escribir(array)
+            englishToggle.classList.remove("active");
+            setTimeout(()=>{escribir(array)},2000)
         //nav
         cambiaIdioma('nav-inicio','<b><</b>Inicio<b>></b>')
         cambiaIdioma('nav-sobremi','<b><</b>Sobre Mi<b>></b>') 
         cambiaIdioma('nav-tecnologias','<b><</b>Tecnologias<b>></b>') 
         cambiaIdioma('nav-contacto','<b><</b>Contacto<b>></b>') 
+        //inicio
         cambiaIdioma("title_descript","<b class='b3'><</b>Hola, soy Luciano Chiroli<b class='b3'>/></b>")
         cambiaIdioma("contactame","Contáctame <b class='b5'><i class='uil uil-message button__icon'></i></b>")
         cambiaIdioma("sTitle",`<b class='b1'>class =</b> "Desarrollador Front End";`);
+        //description
         cambiaIdioma("description","<b class='b3'>let</b> <b class='b2'>descripción</b> = ")
-       
+        cambiaIdioma("sobreMiTitle",`<b class="b2"><</b>Sobre Mi<b class="b2">/></b>`)
+        cambiaIdioma("aboutDescription",`soy de Mendoza,Argentina. Actualmente tengo 20 años y estoy realizando la Tecnicatura universitaria en programación,`)
+        cambiaIdioma("readMoreSpan",`en la UTN Facultad Regional Mendoza. Soy una persona autodidacta, 
+        perseverante y en busca de proyectos que me ayuden a crecer profesionalmente<b class="b4">"</b>`)
+        cambiaIdioma("readMoreD",`Leer Más <i class="uil uil-arrow-down"></i>` )
+        cambiaIdioma("readMenosD",`Leer Menos <i class="uil uil-arrow-up"></i>` )
+        //downloadCv
+        cambiaIdioma("downloadCv",`Descargar CV <b class="b5"><i class="uil uil-download-alt button__icon"></i></b></a>`)
+        //education
+        cambiaIdioma("titleEducation",` <b class="b2"><</b>Educación<b class="b2">/></b>`)
+        cambiaIdioma("soporteTec","Soporte Tecnico, Jr. Achivement 2021/2021")
+        cambiaIdioma("titleCursos",`<b class="b2"><</b>Cursos Web<b class="b2">/>`)
+        cambiaIdioma("secundario", `Colegio Secundario, Gral Jose De San MartinN°9-001/2018<`)
+        cambiaIdioma("leerMasCursos",`Leer Más <i class="uil uil-arrow-down"></i>` )
+        cambiaIdioma("leerMenosCursos",`Leer Menos <i class="uil uil-arrow-up"></i>` )
+        //tecnologias
+        cambiaIdioma("tecnoTitle",`<b class="b2"><</b>Tecnologies<b class="b2">/></b>` )
+        cambiaIdioma("descriptTec",`Poseo conocimientos en maquetado web,desarrollo de interfaces de usuario, 
+        en las tecnologías actuales para un desarrollador frontend, tales como HTML5, CSS, Bootstrap,`)
+        cambiaIdioma("readMoreSpanT",`JavaScript Vanilla y ReactJs. <br>
+        También poseo conocimientos en el lado del backend con node.js y express, ademas de contar con conocimientos en bases de datos relacionales como SQL y no relacionales como Mongo DB<b class="b4">"</b>`)
+        cambiaIdioma("readMoreMasT",`Leer Mas <i class="uil uil-arrow-down"></i>`)
+        cambiaIdioma("readMenosT",`Leer Menos <i class="uil uil-arrow-down"></i>`)
+        //contactme
+        cambiaIdioma("contactMe",`<b class="b2"> <</b> Contáctame<b class="b2">/></b>`)
+        cambiaIdioma("labNombre",`Nombre:`)
+        cambiaIdioma("labAsunto",`Asunto:`)
+        cambiaIdioma("labMensaje",`Mensaje:`)
+        cambiaIdioma("buttonSubmit",`Enviar <b class="b5"> <i class="uil uil-message button__icon">`)
+        
     }
 })
-
+const label = document.getElementById('labNombre');
+console.log(label.innerHTML)
 
 function cambiaIdioma (id, cadena = ""){
     let nombre = document.getElementById(id);
